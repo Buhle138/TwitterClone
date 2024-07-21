1d7780aa712e62a944d32bb61dab3a7bf02f1c0c
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-21 20:18:28 +0200

    Initial Commit

---
+++ TwitterClone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TwitterClone
+//
+//  Created by Buhle Radzilani on 2024/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Buhle Radzilani on 2024/07/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

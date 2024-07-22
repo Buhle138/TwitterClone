74aad76f31002a26cdf08910aacab397f6bd064b
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-22 12:34:14 +0200

    creating the notifications view.

---
+++ TwitterClone/Core/Notifications/Notifications.swift
@@ -0,0 +1,20 @@
+//
+//  Notifications.swift
+//  TwitterClone
+//
+//  Created by Buhle Radzilani on 2024/07/22.
+//
+
+import SwiftUI
+
+struct Notifications: View {
+    var body: some View {
+        Text("Notification")
+    }
+}
+
+struct Notifications_Previews: PreviewProvider {
+    static var previews: some View {
+        Notifications()
+    }
+}

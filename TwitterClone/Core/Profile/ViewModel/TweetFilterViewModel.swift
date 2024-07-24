39ba99fe8358c2ffce2224f1070929e0f2219331
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-24 16:25:18 +0200

    creating the tweets view model.

---
+++ TwitterClone/Core/Profile/ViewModel/TweetFilterViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TweetFilterViewModel.swift
+//  TwitterClone
+//
+//  Created by Buhle Radzilani on 2024/07/24.
+//
+
+import Foundation
+
+enum TweetFilterViewModel: Int, CaseIterable {
+    case tweets
+    case replies
+    case likes
+}
--- TwitterClone/Core/Profile/Views/ProfileView.swift
+++ TwitterClone/Core/Profile/Views/ProfileView.swift
@@ -17,6 +17,8 @@ struct ProfileView: View {
            
             userInfoDetails
             
+            
+            
             Spacer()
         }
     }

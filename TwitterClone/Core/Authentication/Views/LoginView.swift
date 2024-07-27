457f100f2beb0f6fed676ba6a1bce6a4815867c2
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-27 23:27:51 +0200

    Creating the login and registration view.

---
+++ TwitterClone/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  TwitterClone
+//
+//  Created by Buhle Radzilani on 2024/07/27.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}

e4a36a9fe111e31aeb186c6d80aecda29bc01f15
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-28 22:55:55 +0200

    custom input fields complete.

--- TwitterClone/Core/Components/CustomInputFields.swift
+++ TwitterClone/Core/Components/CustomInputFields.swift
@@ -8,13 +8,29 @@
 import SwiftUI
 
 struct CustomInputFields: View {
+    let imageName: String
+    let placeholderText: String
+    @Binding var text: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Image(systemName: imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(Color(.darkGray))
+                
+                TextField(placeholderText, text: $text)
+            }
+            Divider()
+                .background(Color(.darkGray))   
+        }
     }
 }
 
 struct CustomInputFields_Previews: PreviewProvider {
     static var previews: some View {
-        CustomInputFields()
+        CustomInputFields(imageName: "envelope", placeholderText: "Email", text: .constant(""))
     }
 }

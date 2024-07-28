78ccb25c98791a1108cf273d847cc56cd47f480d
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-28 22:52:13 +0200

    creating a custom input field in the components folder so that we can re use it throughout the application.

--- TwitterClone/Core/Authentication/Views/LoginView.swift
+++ TwitterClone/Core/Authentication/Views/LoginView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+
     var body: some View {
         //parent container
         VStack {
@@ -29,9 +32,65 @@ struct LoginView: View {
             .foregroundColor(.white)
             .clipShape(RoundedShape(corners: .bottomRight))
             
+            VStack(spacing: 40) {
+                TextField("Email", text: $email)
+                
+                TextField("Password", text: $password)
+            }
+            .padding(.horizontal, 32)
+            .padding(.top, 44)
+            
+            HStack {
+                Spacer()
+                
+                NavigationLink {
+                    Text("Reset password view...")
+                } label: {
+                    Text("Forget Password")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(.systemBlue))
+                        .padding(.top)
+                        .padding(.trailing, 24)
+                }
+
+            }
+            
+            Button {
+                print("Sign in here...")
+            } label: {
+                Text("Sign in")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 340, height: 50)
+                    .background(Color(.systemBlue))
+                    .clipShape(Capsule())
+                    .padding()
+            }
+            .shadow(color: .gray.opacity(0.5), radius: 100, x: 0, y: 0)
+
+            
+            
             Spacer()
+            
+            NavigationLink {
+                RegistrationView()
+                    .navigationBarHidden(true)
+            } label: {
+                HStack {
+                    Text("Don't have an account")
+                        .font(.footnote)
+                    Text("Sign up")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                }
+            }
+            .padding(.bottom, 32)
+            .foregroundColor(Color(.systemBlue))
+
         }
         .ignoresSafeArea()
+        .navigationBarHidden(true)
     }
 }
 
---
+++ TwitterClone/Core/Components/CustomInputFields.swift
@@ -0,0 +1,20 @@
+//
+//  CustomInputFields.swift
+//  TwitterClone
+//
+//  Created by Buhle Radzilani on 2024/07/28.
+//
+
+import SwiftUI
+
+struct CustomInputFields: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomInputFields_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomInputFields()
+    }
+}

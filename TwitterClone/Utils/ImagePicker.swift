945d9773bd3b0296318c08d341039b987b76d288
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-31 18:18:56 +0200

    Creating the image picker for the user to be able to choose a profile image.

---
+++ TwitterClone/Utils/ImagePicker.swift
@@ -0,0 +1,28 @@
+//
+//  ImagePicker.swift
+//  TwitterClone
+//
+//  Created by Buhle Radzilani on 2024/07/31.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    func makeCoordinator() -> Coordinator {
+        
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
+
+
+extension ImagePicker {
+    
+}

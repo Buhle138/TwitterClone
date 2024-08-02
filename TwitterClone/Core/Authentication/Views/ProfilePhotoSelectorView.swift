489f5befb7488f8f9c3a1c51563af058782414bd
buhleradzilani <radzilanibuhle@gmail.com>
2024-08-02 21:27:23 +0200

    adding the continue button below when the image is selected.

--- TwitterClone/Core/Authentication/Views/ProfilePhotoSelectorView.swift
+++ TwitterClone/Core/Authentication/Views/ProfilePhotoSelectorView.swift
@@ -43,6 +43,20 @@ struct ProfilePhotoSelectorView: View {
                ImagePicker(selectedImage: $selectedImage)
             }
 
+            if  profileImage != nil {
+                Button {
+                 print("DEBUG: Finish registering user...")
+                } label: {
+                    Text("Continue")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 340, height: 50)
+                        .background(Color(.systemBlue))
+                        .clipShape(Capsule())
+                        .padding()
+                }
+                .shadow(color: .gray.opacity(0.5), radius: 100, x: 0, y: 0)
+            }
             
             Spacer()
         }

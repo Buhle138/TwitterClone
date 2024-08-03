138ca8078cdbb0cefa4d5bb93776a85903d5b02a
buhleradzilani <radzilanibuhle@gmail.com>
2024-08-03 15:32:32 +0200

    Adding the king fisher library in order to read the image url of the user from the firebase database.

--- TwitterClone/Core/SideMenu/Views/SideMenuView.swift
+++ TwitterClone/Core/SideMenu/Views/SideMenuView.swift
@@ -6,12 +6,58 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct SideMenuView: View {
     @EnvironmentObject var authViewModel: AuthViewModel
     var body: some View {
 
-        if let user = authViewModel.currentUser
+        if let user = authViewModel.currentUser {
+            VStack(alignment: .leading, spacing: 32) {
+                VStack(alignment: .leading) {
+                    KFImage(URL(string: user.profileImageUrl))
+                        .resizable()
+                        .scaledToFill()
+                        .clipShape(Circle())
+                        .frame(width: 48, height: 48)
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text(user.fullname)
+                            .font(.headline)
+                        
+                        Text("@\(user.username)")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    
+                    UserStatsView()
+                        .padding(.vertical)
+                }
+                .padding(.leading)
+                
+                ForEach(SideMenuViewModel.allCases, id: \.rawValue) { viewModel in
+                    if viewModel == .profile {
+                        NavigationLink  {
+                            ProfileView()
+                        } label: {
+                            SideMenuOptionRowView(viewModel: viewModel)
+                        }
+                    } else if viewModel == .logout {
+                        Button {
+                            authViewModel.signOut()
+                        } label: {
+                            SideMenuOptionRowView(viewModel: viewModel)
+                        }
+
+                    }else {
+                        SideMenuOptionRowView(viewModel: viewModel)
+                    }
+                   
+                }
+               
+                Spacer()
+            }
+        }
        
     }
 }
